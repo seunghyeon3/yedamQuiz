ec03c5a6b639c55cd203d3bd20adac59458baa4f
cyannara <cyannara@naver.com>
2022-05-27 17:18:40 +0900

    메세지 소스 추가

--- quiz/src/main/java/com/yedam/quiz/QuizApplication.java
+++ quiz/src/main/java/com/yedam/quiz/QuizApplication.java
@@ -1,11 +1,16 @@
 package com.yedam.quiz;
 
+import java.util.Locale;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @Controller
 @SpringBootApplication
@@ -13,6 +18,13 @@ public class QuizApplication {
     private static final Logger log = LoggerFactory.getLogger(QuizApplication.class);
     public static void main(String[] args) {
         SpringApplication.run(QuizApplication.class, args);
+    }
+    
+    @Bean
+    public LocaleResolver localeResolver() {
+    	SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+    	sessionLocaleResolver.setDefaultLocale(Locale.KOREA);
+    	return sessionLocaleResolver;
     }
     
     @RequestMapping("/")
--- quiz/src/main/java/com/yedam/quiz/quiz/service/QuizVO.java
+++ quiz/src/main/java/com/yedam/quiz/quiz/service/QuizVO.java
@@ -4,11 +4,18 @@ import lombok.Data;
 
 @Data
 public class QuizVO {
-	private String quizNo;
-	private String sbjtCd;
-	private String quizCtnt;
-	private String typCd;
-	private String ansr;
-	private String desct;
-	private String useYn;
+	private String quizNo;		//문제번호
+	private String sbjtCd;		//과목코드
+	private String sbjtNm;		//과목명
+	private String quizCtnt;	//문제
+	private String typCd;		//문제유형코드
+	private String typNm;		//문제유형명
+	private String ansr;		//정답
+	private String desct;		//문제해설
+	private String useYn;		//사용여부
+	private String questCnt;	//출제수
+	private String rightRate;   //정답율
+	private String wdt;			//등록일자
+	private String wname;		//등록자
+	
 }
