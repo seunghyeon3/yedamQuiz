7c5317ea8679edec0c5af1300c29fa072d816f95
seunghyeon <psh@yedam.ac.kr>
2022-07-01 18:27:38 +0900

    0710

--- quiz/src/main/java/com/yedam/quiz/exam/web/ExamController.java
+++ quiz/src/main/java/com/yedam/quiz/exam/web/ExamController.java
@@ -6,15 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.yedam.quiz.exam.service.ExamService;
 import com.yedam.quiz.exam.service.ExamVO;
-import com.yedam.quiz.exam.service.StudentService;
 import com.yedam.quiz.quiz.service.QuizService;
 import com.yedam.quiz.student.service.StudentService;
-import com.yedam.quiz.student.service.StudentVO;
 
 @Controller
 public class ExamController {
@@ -26,22 +22,13 @@ public class ExamController {
 	private QuizService quizService;
 	
 	@Autowired
-<<<<<<< HEAD
 	private StudentService studentService;
-	
-=======
-	private StudentService stuService;
->>>>>>> e37f085fcf6358ec84cecdea5bd92b4132d180ad
 	
 	@RequestMapping("examRegistForm")
 	public String examRegistForm(Model model) {
 		model.addAttribute("quizList", quizService.getQuizList(null));
 		//훈련생 리스트
-<<<<<<< HEAD
 		model.addAttribute("studentList", studentService.getStudentList());
-=======
-		model.addAttribute("studentList", stuService.getStudentList());
->>>>>>> e37f085fcf6358ec84cecdea5bd92b4132d180ad
 		return "exam/examRegist";
 	}
 	
