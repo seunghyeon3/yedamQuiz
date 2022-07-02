d79c4dadca5f09bb57ba7949f182177f61ebf5b3
'cholee' <'cholee@yedam.ac'>
2022-07-02 12:30:44 +0900

    Merge branch 'chart' of https://github.com/seunghyeon3/yedamQuiz.git into chart

---
+++ quiz/src/main/java/com/yedam/quiz/statics/service/serviceImpl/StaticsServiceImpl.java
@@ -0,0 +1,5 @@
+package com.yedam.quiz.statics.service.serviceImpl;
+
+public class StaticsServiceImpl {
+
+}
--- quiz/src/main/java/com/yedam/quiz/statics/web/StaticsController.java
+++ quiz/src/main/java/com/yedam/quiz/statics/web/StaticsController.java
@@ -1,14 +1,13 @@
 package com.yedam.quiz.statics.web;
 
 import org.springframework.stereotype.Controller;
-
 /*
  * 작성자 : 이창호
- * 작성일자 : 2022-06-28 Tue.
- * 통계정보 관리 : 문제별, 학생별 통계 정보.
+ * 작성일자 : 2022-07-01 
+ * 문제별 통계정보 : 문제별 통계 데이터.
  */
 
 @Controller
 public class StaticsController {
-
+	
 }
